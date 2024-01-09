b12326c6fc3f988991bcf2409c0cb85b41723f41
Rafał Siwek <rafal.siwek@o2.pl>
2024-01-09 01:09:47 +0100

    use Microk8s 1.28 version

--- .github/workflows/setup_microk8s.sh
+++ .github/workflows/setup_microk8s.sh
@@ -5,7 +5,7 @@ microk8s enable dns storage metallb:10.64.140.43-10.64.140.49
 microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
 
 # Juju controller + model
-juju bootstrap --agent-version=2.9.34 --no-gui microk8s uk8sx
+juju bootstrap --no-gui microk8s uk8sx
 juju add-model kubeflow
 
 # Install + setup kubectl
