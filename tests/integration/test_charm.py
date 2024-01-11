d919c0c58a4cf18ccd882b84db2b9f3ae661d3eb
Rafał Siwek <rafal.siwek@o2.pl>
2024-01-11 12:14:05 +0100

    add error handling

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import pytest
 import yaml
+from asyncio.exceptions import TimeoutError
 from pytest_operator.plugin import OpsTest
 from tenacity import retry, stop_after_attempt
 
@@ -19,27 +20,29 @@ class TestCharm:
     @retry(stop=stop_after_attempt(5))
     async def _deploy_relational_db(self, ops_test: OpsTest):
         if RELATIONAL_DB_CHARM_NAME in ops_test.model.applications.keys():
-            logger.info(f"Failed to deploy {RELATIONAL_DB_CHARM_NAME}, redeploying...")
+            logger.info(f"Redeploying...")
             await ops_test.model.remove_application(
                 app_name=RELATIONAL_DB_CHARM_NAME,
                 force=True,
                 destroy_storage=True,
                 no_wait=True,
             )
-
+        
         await ops_test.model.deploy(
             RELATIONAL_DB_CHARM_NAME,
             channel="8.0/stable",
             trust=True,
         )
-
-        await ops_test.model.wait_for_idle(
-            apps=[RELATIONAL_DB_CHARM_NAME],
-            status="active",
-            raise_on_blocked=False,
-            raise_on_error=False,
-            timeout=30,
-        )
+        try:    
+            await ops_test.model.wait_for_idle(
+                apps=[RELATIONAL_DB_CHARM_NAME],
+                status="active",
+                raise_on_blocked=False,
+                raise_on_error=False,
+                timeout=30,
+            )
+        except TimeoutError:
+            raise TimeoutError(f"Failed to deploy {RELATIONAL_DB_CHARM_NAME}")
 
     @pytest.mark.abort_on_fail
     @pytest.mark.skip_if_deployed
